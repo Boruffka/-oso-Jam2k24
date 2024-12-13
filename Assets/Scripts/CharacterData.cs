f64395de2028eeec4e046c2c20460df737e22980
Boruffka <119352157+Boruffka@users.noreply.github.com>
2024-12-13 23:00:24 +0100

    textInputStart
    :(

--- Assets/Scripts/CharacterData.cs
+++ Assets/Scripts/CharacterData.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 
 public class CharacterData
 {
-    List<string> introDialogue;         // lines of intro dialogue (judge)
+    List<string> introDialogue;         // [1] lines of intro dialogue (judge)
 
-    string characterName;               // single line
+    string characterName;               // [2] single line
 
     Sprite characterSprite;             // character sprite
 
-    List<string> entryDialogue;         // lines of entry dialogue
+    List<string> entryDialogue;         // [3] lines of entry dialogue
 
-    string questionOne;                 // 
-    string questionTwo;                 // two questions, short
+    string questionOne;                 // [4]
+    string questionTwo;                 // [5] two questions, short
 
-    List<string> responseOneDialogue;   // response to question one
-    List<string> responseTwoDialogue;   // response to question two
+    List<string> responseOneDialogue;   // [6] response to question one
+    List<string> responseTwoDialogue;   // [7] response to question two
 
     string filesInfo;                   // box of info +/- 14 lines
 
--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -4,19 +4,39 @@ using System.IO;
 using System.Collections.Generic;
 public class TextManager : MonoBehaviour
 {
-    private Dictionary<int, CharacterData> charactersLoaded;
+    private List<CharacterData> charactersLoaded;
 
     void Start()
     {
         // name the file "dialogueData.txt"
-        ReadFile();
+        LoadCharacters();
     }
 
-    void ReadFile()
+    // s - single, m - multiple lines
+    // [0] intro (m)
+    // [1] charName (s)
+    // [2] entry (m)
+    // [3] question1 (s)
+    // [4] question2 (s)
+    // [5] response1 (m)
+    // [6] response2 (m)
+    // [7] filesInfo (s)
+    // [8] guilty (s) 0 or 1
+
+    void LoadCharacters()
     {
         string path = "Assets/dialogueData.txt";
         StreamReader reader = new StreamReader(path);
-        Debug.Log(reader.ReadToEnd());
+        string loadedData = reader.ReadToEnd();
+        string loadedLine = "";
+        int inputField = 0;
+        for (int i = 0; i < loadedData.Length; i++)
+        {
+            switch(inputField)
+            {
+
+            }
+        }
         reader.Close();
     }
 }
